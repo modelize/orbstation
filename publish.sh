8da098043ef9063a8b2fe8c704697da618a70c0a
Michael Becker <michael@bemit.codes>
2023-01-01 17:36:36 +0100

    ci

--- publish.sh
+++ publish.sh
@@ -6,16 +6,20 @@ git checkout -- packages/webpackPartialConfig.js
 
 cd ./packages
 
-cd orb-command
-cp README.md build/ && cp ../../LICENSE build/
+cd orb-route
+cp README.md build/ && cp LICENSE build/
 cd ../
 
-cd orb-route
-cp README.md build/ && cp ../../LICENSE build/
+cd orb-command
+cp README.md build/ && cp LICENSE build/
+cd ../
+
+cd orb-cling
+cp README.md build/ && cp LICENSE build/
 cd ../
 
 cd schema-lib
-cp README.md build/ && cp ../../LICENSE build/
+cp README.md build/ && cp LICENSE build/
 cd ../
 
 cd ../
